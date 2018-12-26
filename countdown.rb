411a05c04ec707c56e5b791048fa89b5c516801c
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-26 01:59:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,8 +8,9 @@ def countdown(number)
  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
+def countdown_with_sleep(number(1))
   while number > 0
+  puts "#{number} SECOND(S)!"
   number -= 1
  end
 end
